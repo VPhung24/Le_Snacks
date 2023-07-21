18d67a6fb78c63612f176802d0b723a523d6dbcf
Vivian Phung <ethglobalparis@vivianphung.com>
2023-07-21 22:44:08 +0200

    init

---
+++ LeSnacks/LeSnacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeSnacksApp.swift
+//  LeSnacks
+//
+//  Created by Vivian Phung on 7/21/23.
+//
+
+import SwiftUI
+
+@main
+struct LeSnacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
